24b90de2d30b03fa389df5580e29d76c530b8b0e
lizhiwei <lizhiwei523@gmail.com>
2022-09-26 22:05:55 +0800

    1

---
+++ code/fy0929/src/main/java/com/fy/fy0929/controller/IndexController.java
@@ -0,0 +1,16 @@
+package com.fy.fy0929.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class IndexController {
+    @RequestMapping({"/",""})
+    public String index(HttpServletRequest request,Model model){
+        model.addAttribute("contextPath",request.getContextPath());
+        return "index";
+    }
+}
